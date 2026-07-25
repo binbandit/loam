515dda7344646154885b44e799d0ffed9ccabea8
BinBandit <crazywolf132@gmail.com>
2026-07-25 18:43:32 +1000

    test(desktop): add jsdom range layout polyfill

--- apps/desktop/src/test-setup.ts
+++ apps/desktop/src/test-setup.ts
@@ -5,6 +5,13 @@ Element.prototype.setPointerCapture = () => {};
 Element.prototype.releasePointerCapture = () => {};
 Element.prototype.hasPointerCapture = () => false;
 
+// jsdom's Range has no layout; CM6 measures text with it whenever a gutter
+// is present. Zero rects are the right answer in a zero-size document.
+if (!Range.prototype.getClientRects) {
+  Range.prototype.getClientRects = () => Object.assign([], { item: () => null }) as DOMRectList;
+  Range.prototype.getBoundingClientRect = () => new DOMRect();
+}
+
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
